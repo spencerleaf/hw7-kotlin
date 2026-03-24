8bf3271d9e242ae36755c9637dd6f0d7a542d2bb
spencerleaf <therealspencerleaf@gmail.com>
2026-03-24 00:42:29 -0700

    eval

--- main.kt
+++ main.kt
@@ -7,10 +7,22 @@ sealed class MathExpressions{
     data class Mul(val left: MathExpressions, val right: MathExpressions) : MathExpressions()
     data class Div(val left: MathExpressions, val right: MathExpressions) : MathExpressions()
     data class Pow(val base: MathExpressions, val exp: MathExpressions) : MathExpressions()
+}
 
+fun eval(e: MathExpressions): Int = when(e) {
+    is MathExpressions.Num -> e.value
+    is MathExpressions.Variable -> throw Exception("trying to eval a variable")
+    is MathExpressions.Negative -> -eval(e.name)
+    is MathExpressions.Add -> eval(e.left) + eval(e.right)
+    is MathExpressions.Sub -> eval(e.left) - eval(e.right)
+    is MathExpressions.Mul -> eval(e.left) * eval(e.right)
+    is MathExpressions.Div -> eval(e.left) / eval(e.right)
+    is MathExpressions.Pow -> Math.pow(eval(e.base).toDouble(), eval(e.exp).toDouble()).toInt()
 }
 
 fun main(){
     val x = MathExpressions.Num(5)
     println(x)
+    val expr = MathExpressions.Add(MathExpressions.Num(3), MathExpressions.Mul(MathExpressions.Num(2), MathExpressions.Num(4)))
+    println(eval(expr))
 }
