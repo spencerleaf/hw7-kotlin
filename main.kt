b510921f752a68d6f5d8df27dde3f38cdaffc7d6
spencerleaf <therealspencerleaf@gmail.com>
2026-03-24 00:12:30 -0700

    finished sealed class

--- main.kt
+++ main.kt
@@ -1,10 +1,13 @@
 sealed class MathExpressions{
     data class Num(val value: Int) : MathExpressions()
     data class Variable(val name: String) : MathExpressions()
+    data class Negative(val name: MathExpressions) : MathExpressions()
     data class Add(val left: MathExpressions, val right: MathExpressions) : MathExpressions()
     data class Sub(val left: MathExpressions, val right: MathExpressions) : MathExpressions()
     data class Mul(val left: MathExpressions, val right: MathExpressions) : MathExpressions()
     data class Div(val left: MathExpressions, val right: MathExpressions) : MathExpressions()
+    data class Pow(val base: MathExpressions, val exp: MathExpressions) : MathExpressions()
+
 }
 
 fun main(){
