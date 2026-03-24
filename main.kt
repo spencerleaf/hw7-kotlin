3ebc71fc31b65e39f08c8626af32f626fdf87bab
spencerleaf <therealspencerleaf@gmail.com>
2026-03-23 23:44:16 -0700

    whoops forgot to start git

---
+++ main.kt
@@ -0,0 +1,13 @@
+sealed class MathExpressions{
+    data class Num(val value: Int) : MathExpressions()
+    data class Variable(val name: String) : MathExpressions()
+    data class Add(val left: MathExpressions, val right: MathExpressions) : MathExpressions()
+    data class Sub(val left: MathExpressions, val right: MathExpressions) : MathExpressions()
+    data class Mul(val left: MathExpressions, val right: MathExpressions) : MathExpressions()
+    data class Div(val left: MathExpressions, val right: MathExpressions) : MathExpressions()
+}
+
+fun main(){
+    val x = MathExpressions.Num(5)
+    println(x)
+}
