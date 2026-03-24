229e8b653dc87caec70d08a745b41860566c8d7a
spencerleaf <therealspencerleaf@gmail.com>
2026-03-24 00:56:48 -0700

    structure for simplify

--- main.kt
+++ main.kt
@@ -20,9 +20,31 @@ fun eval(e: MathExpressions): Int = when(e) {
     is MathExpressions.Pow -> Math.pow(eval(e.base).toDouble(), eval(e.exp).toDouble()).toInt()
 }
 
+fun simplify(e: MathExpressions): MathExpressions = when(e){
+    is MathExpressions.Num -> e
+    is MathExpressions.Variable -> e
+    is MathExpressions.Negative ->
+    {
+        val s = simplify(e.name)
+        when (s){
+            is MathExpressions.Num -> MathExpressions.Num(-s.value)
+            is MathExpressions.Negative -> s.name
+            else -> MathExpressions.Negative(s)
+        }
+    }
+    is MathExpressions.Add -> simplifyAdd(simplify(e.left), simplify(e.right))
+    is MathExpressions.Sub -> simplifySub(simplify(e.left), simplify(e.right))
+    is MathExpressions.Mul -> simplifyMul(simplify(e.left), simplify(e.right))
+    is MathExpressions.Div -> simplifyDiv(simplify(e.left), simplify(e.right))
+    is MathExpressions.Pow -> simplifyPow(simplify(e.left), simplify(e.right))
+}
 fun main(){
     val x = MathExpressions.Num(5)
     println(x)
     val expr = MathExpressions.Add(MathExpressions.Num(3), MathExpressions.Mul(MathExpressions.Num(2), MathExpressions.Num(4)))
     println(eval(expr))
+    val expr2 = MathExpressions.Pow(MathExpressions.Num(2), MathExpressions.Num(3))
+    println(eval(expr2))
+    val expr3 = MathExpressions.Negative(MathExpressions.Num(5))
+    println(eval(expr3))
 }
